0e64d8b2d7d0d953ed9d7a943e9d6bd584d7aa99
Vivek <yadav100adu@gmail.com>
2025-02-17 15:20:33 +0530

    new

---
+++ p2.js
@@ -0,0 +1,4 @@
+const fs = require('fs');
+
+const data="hg8f86";
+fs.writeFileSync("./data.txt",data);
---
+++ p5.js
@@ -0,0 +1,28 @@
+const fs=require('fs');
+
+// const read=()=>{
+//     fs.readFile("./data.txt","utf-8",(err,data)=>{
+//         if(err)
+//             console.log(err)
+//         else
+//             console.log(data)
+//     });
+// }
+
+// const write=()=>{
+//     const data="hg8f86";
+//     fs.writeFileSync("./data.txt",data);
+// }
+const write=()=>{
+    const data="hg86";
+    fs.writeFile("./data.txt",data,"utf-8",(err)=>{
+        if(err)
+            console.log(err)
+        else
+            console.log("writen")
+    });
+}
+console.log("after reading")
+write();
+// read();
+console.log("before reading")
