735968775a9a36b856fc04e3717ff63eeedd1323
Vivek <yadav100adu@gmail.com>
2025-02-20 14:27:21 +0530

    me

--- p7.js
+++ p7.js
@@ -7,3 +7,10 @@ fs.mkdir("mydir",(err)=>{
 
 const data="Vivek Kumar";
 fs.writeFileSync("./mydir/data.txt",data);
+
+fs.unlink("./mydir/data.txt",(err)=>{
+    if(err)
+        console.log(err)
+    else
+        console.log('file deleted')
+})
